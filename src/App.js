49905a12fb3b5196d4f3bd9290e8b440a5bfc72f
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-24 16:16:30 -0400

    finished half of part4

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import './App.css';
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-
+import ListItem from './components/ListItem';
 
 
 function App() {
-
   const [students, setStudents] = useState([])
   const [searchText, setSearchText] = useState('')
-  const [openIndex, setOpenIndex] = useState([]);
+  const url = 'https://api.hatchways.io/assessment/students'
 
   useEffect(() => {
     const fetchStudents = async () => {
-      const studentsRes = await axios.get('https://api.hatchways.io/assessment/students')
+      const studentsRes = await axios.get(url)
       setStudents(studentsRes.data.students)
     }
     fetchStudents()
   }, [])
 
   const calcAverage = (array) => array.reduce((a, b) => parseInt(a) + parseInt(b)) / array.length;
-
-  const makeHandleClick = (i) => {
-    if (openIndex.includes(i)) {
-      setOpenIndex(openIndex.filter(item => item !== i))
-    } else {
-      setOpenIndex(newArray => [...newArray, i]);
-    }
-  }
 
   return (
     <div className='main__container'>
@@ -39,38 +30,20 @@ function App() {
           onChange={(e) => setSearchText(e.target.value)}
         />
 
-        {students.filter((student) => {
-          if (searchText === '') {
-            return student
-          } else if (student.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
-            student.lastName.toLowerCase().includes(searchText.toLowerCase())) {
-            return student
-          }
-        }).map((student, i) => (
-          <div className="students" key={student.id}>
-            <div className='image__container'>
-              <img className='students__image' src={student.pic} alt="avatar of various students"></img>
-            </div>
-            <div className='text__container'>
-              <h3 className='students__title'>{student.firstName} {student.lastName}
-                <span onClick={() => makeHandleClick(i)} className='plus-minus'>
-                  {openIndex.includes(i) ? '\u2796' : '\u2795'}
-                </span></h3>
+        <input
+          type='text'
+          className='search__input'
+          placeholder='Search by tag'
 
-              <p>Email: {student.email}</p>
-              <p>Company: {student.company}</p>
-              <p>Skill: {student.skill}</p>
-              <p>Average: {calcAverage(student.grades)}%</p>
-              <ul className={`grades__list`}>
-                {openIndex.includes(i) ?
-                  student.grades.map((grade, idx) => (
-                    <li key={idx}>Test {idx + 1}: <span className='grades__span'>{grade}%</span></li>
-                  ))
-                  :
-                  null}
-              </ul>
-            </div>
-          </div>
+        />
+
+        {students.filter((student) => {
+          let [fName, lName] = [student.firstName.toLowerCase(), student.lastName.toLowerCase()];
+          let includesSearch = fName.includes(searchText.toLowerCase()) || lName.includes(searchText.toLowerCase())
+
+          return searchText === '' ? student : (includesSearch ? student : null)
+        }).map((student, i) => (
+          <ListItem student={student} i={i} calcAverage={calcAverage} />
         ))}
       </div>
     </div>
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,1 +1,66 @@
+import { useState } from 'react';
 
+const ListItem = ({ student, i, calcAverage }) => {
+  const [openIndex, setOpenIndex] = useState([]);
+
+  const [tag, setTag] = useState('')
+  const [tagArray, setTagArray] = useState([])
+
+  const makeHandleClick = (i) => {
+    if (openIndex.includes(i)) {
+      setOpenIndex(openIndex.filter(item => item !== i))
+    } else {
+      setOpenIndex(newArray => [...newArray, i]);
+    }
+  }
+
+  const handleSubmit = (e, idx) => {
+    e.preventDefault()
+    setTagArray(tagArrayClone => [...tagArrayClone, tag])
+    setTag('')
+  }
+  return (
+    <div className="students" key={student.id}>
+      <div className='image__container'>
+        <img className='students__image' src={student.pic} alt="avatar of various students"></img>
+      </div>
+      <div className='text__container'>
+        <h3 className='students__title'>{student.firstName} {student.lastName}
+          <span onClick={() => makeHandleClick(i)} className='plus-minus'>
+            {openIndex.includes(i) ? '\u2796' : '\u2795'}
+          </span></h3>
+
+        <p>Email: {student.email}</p>
+        <p>Company: {student.company}</p>
+        <p>Skill: {student.skill}</p>
+        <p>Average: {calcAverage(student.grades)}%</p>
+        <ul className={`grades__list`}>
+          {openIndex.includes(i) ?
+            student.grades.map((grade, idx) => (
+              <li key={idx}>Test {idx + 1}: <span className='grades__span'>{grade}%</span></li>
+            ))
+            :
+            null}
+        </ul>
+        <div className='tag__container'>
+          {tagArray.length > 0 ? tagArray.map((tag, i) => (
+            <span key={i} className='tag__span'>{tag}</span>
+          )) : null}
+        </div>
+        <form onSubmit={(e) => handleSubmit(e, i)}>
+          <input
+            name={`tag_input_${student.id}`}
+            type='text'
+            placeholder='Add a tag'
+            className='tag__input'
+            value={tag}
+            onChange={(e) => setTag(e.target.value)}
+          />
+        </form>
+      </div>
+
+    </div>
+  )
+}
+
+export default ListItem
