8bbd9121eeffd4bb5ef400bb32dbc26eab4e47e1
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-30 01:11:28 -0400

    able to add new profile

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import { useEffect, useState } from 'react'
 import ListItem from './components/ListItem';
 
 
+
 function App() {
   const [students, setStudents] = useState([])
   const [searchText, setSearchText] = useState('')
+  const [profile, setProfile] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    age: 0,
+    avatar: ''
+  })
   const url = 'http://localhost:3001/profiles'
 
 
@@ -25,7 +33,40 @@ function App() {
     fetchProfiles()
   }, [])
 
+  const clearFields = () => {
+    setProfile({
+      firstName: '',
+      lastName: '',
+      email: '',
+      age: 0,
+      avatar: ''
+    })
+  }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: profile.firstName,
+          lastName: profile.lastName,
+          email: profile.email,
+          age: profile.age,
+          avatar: profile.avatar
+        })
+      }).then(res => {
+        return res.json()
+      })
+        .then(data => setStudents([...students, data]))
+      clearFields()
+    } catch (err) {
+      console.log('ERROR', err.message)
+    }
+  }
 
   return (
     <div className='main__container'>
@@ -36,6 +77,7 @@ function App() {
           className='search__input'
           placeholder='Search by name'
           value={searchText}
+          name='firstName'
           onChange={(e) => setSearchText(e.target.value)}
         />
 
@@ -65,6 +107,52 @@ function App() {
         }).map((student, i) => (
           <ListItem key={i} tagSearch={tagSearch} student={student} setStudents={setStudents} i={i} />
         ))}
+      </div>
+      <div className='form__container'>
+        <h3 className='form__title'>Add a new profile</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            className='input'
+            placeholder='First name'
+            required='true'
+            value={profile.firstName}
+            onChange={(e) => setProfile({ ...profile, firstName: e.target.value })}
+          />
+          <input
+            type='text'
+            className='input'
+            placeholder='Last name'
+            required='true'
+            value={profile.lastName}
+            onChange={(e) => setProfile({ ...profile, lastName: e.target.value })}
+          />
+          <input
+            type='email'
+            className='input'
+            placeholder='Email'
+            required='true'
+            value={profile.email}
+            onChange={(e) => setProfile({ ...profile, email: e.target.value })}
+          />
+          <input
+            type='number'
+            className='input'
+            placeholder='Age'
+            required='true'
+            value={profile.age}
+            onChange={(e) => setProfile({ ...profile, age: e.target.value })}
+          />
+          <input
+            type='text'
+            className='input'
+            placeholder='Avatar'
+            required='true'
+            value={profile.avatar}
+            onChange={(e) => setProfile({ ...profile, avatar: e.target.value })}
+          />
+          <button className='btn'>Submit</button>
+        </form>
       </div>
     </div>
   );
